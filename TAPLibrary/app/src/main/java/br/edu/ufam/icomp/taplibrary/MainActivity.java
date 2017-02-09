774133bd252d5104e48bd9e98fe7fec482659605
Gabriel Costa <gabrielp.costa@outlook.com>
2017-02-09 19:55:22 -0400

    Alterações na tela de login

--- TAPLibrary/app/src/main/java/br/edu/ufam/icomp/taplibrary/MainActivity.java
+++ TAPLibrary/app/src/main/java/br/edu/ufam/icomp/taplibrary/MainActivity.java
@@ -2,12 +2,50 @@ package br.edu.ufam.icomp.taplibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtUsuario;
+    private EditText edtSenha;
+    private Button btnLogin;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iniciarComponentes();
+    }
+
+    private void iniciarComponentes() {
+        edtUsuario = (EditText) findViewById(R.id.loginUsuario);
+        edtSenha = (EditText) findViewById(R.id.loginSenha);
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnCadastrarOnClick();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnLoginOnClick();
+            }
+        });
+    }
+
+    private void btnCadastrarOnClick() {
+
+    }
+
+    private void btnLoginOnClick() {
+
     }
 }

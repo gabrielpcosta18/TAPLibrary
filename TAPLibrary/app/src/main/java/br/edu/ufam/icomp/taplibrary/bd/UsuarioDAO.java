7ae29cef58d358084e2928efce05d64097712476
Gabriel Costa <gabrielp.costa@outlook.com>
2017-02-09 18:04:46 -0400

    Criação do Projeto

---
+++ TAPLibrary/app/src/main/java/br/edu/ufam/icomp/taplibrary/bd/UsuarioDAO.java
@@ -0,0 +1,8 @@
+package br.edu.ufam.icomp.taplibrary.bd;
+
+/**
+ * Created by gabri on 09/02/2017.
+ */
+
+public class UsuarioDAO {
+}
---
+++ TAPLibrary/app/src/main/java/br/edu/ufam/icomp/taplibrary/model/Usuario.java
@@ -0,0 +1,8 @@
+package br.edu.ufam.icomp.taplibrary.model;
+
+/**
+ * Created by gabri on 09/02/2017.
+ */
+
+public class Usuario {
+}
